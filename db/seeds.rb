347210cebdacb1ecda30998a7bbdb1e71fc70d98
Elton Santos <eltonaxl@hotmail.com>
2016-12-21 23:47:07 -0300

    Generate models Category and Ad and add 10 categories on seed and install gem rails-erd for generate diagram based on application's Active Record models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando as categorias..."
+categories = [ "Animais e acessórios", "Esporte", "Imóveis", "Para a sua casa", "Veículos e barcos", "Música e hobbies", "Bebês e crianças", "Moda e beleza", "Eletrônicos e celulares", "Empregos e negócios" ]
+
+categories.each do |category|
+  Category.find_or_create_by(description: category)
+end
+
+puts "Categorias cadastradas com sucesso!"
