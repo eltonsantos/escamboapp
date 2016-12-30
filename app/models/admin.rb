e4c405a0e8920e39285b5be5106148480af250eb
Elton Santos <eltonaxl@hotmail.com>
2016-12-30 13:40:18 -0300

    Add role in seeds, utils and index and set a enum role on model Admin and a function for adjust the description

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,16 @@
 class Admin < ApplicationRecord
+  enum role: [:full_access, :restricted_access]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def description_role
+    if self.role == 'full_access'
+      'Acesso completo'
+    else
+      'Acesso restrito'
+    end
+  end
+
 end
--- lib/tasks/utils.rake
+++ lib/tasks/utils.rake
@@ -5,7 +5,7 @@ namespace :utils do
     puts "Cadastrando administradores..."
 
     10.times do |i|
-      Admin.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "12345678", password_confirmation: "12345678")
+      Admin.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "12345678", password_confirmation: "12345678", role: 1)
     end
 
     puts "ADMINISTRADORES cadastrados com sucesso!"
